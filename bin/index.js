fc28c72ffb426505b74b47879ff6bf43e543b73c
Jon Fazzaro <JFazzaro@apterainc.com>
2020-05-22 09:28:59 -0400

    fix: oops

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
 const falco = require('../src/falco');
-const cmd = require('../src/cmd');
-
 
 try {
-    console.log(cmd.read("git rev-parse --show-toplevel"));
-    // falco.run(...process.argv.slice(2));
+    falco.run(...process.argv.slice(2));
 } catch (e) {
-    e.message 
+    e.message
         ? console.error(e.message)
         : console.error(e);
 }
